ffcb54ad3ee4c4cc30e12fe3bcd7c3c1d40958f0
KEVINtique351 <kevintique86@gmail.com>
2024-03-05 16:35:51 -0500

    controladores para nevas pestañas

--- resources/views/topbar.blade.php
+++ resources/views/topbar.blade.php
@@ -11,7 +11,7 @@
                     <a class="nav-link" href="#">Inicia seccion</a>
                 </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Reserva</a>
+                <a class="nav-link" href="{{ route('topbar.Reserva') }}">Reserva</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">Caja</a>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\topbar\AuthController;
 use App\Http\Controllers\SalonesController;
 use App\Http\Controllers\ServiController;
 use App\Http\Controllers\ServiciosOtrosController;
+
+use App\Http\Controllers\ReservaController;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +23,5 @@ Route::get('/logados', [AuthController::class, 'logados'])->name('logados');
 Route::get('/salones', [SalonesController::class, 'mostrarSalones'])->name('salones.index');
 Route::get('/servicios', [ServiController::class, 'serviciosVer'])->name('ser.servicio');
 Route::get('/otroServicio', [ServiciosOtrosController::class, 'OtrosVer'])->name('otros.OtroServicios');
+
+Route::get('/reserva', [ReservaController::class, 'reservaSalon'])->name('topbar.Reserva');
